dc3105c716c0a2b888928a46405100dc8c743875
Darling <844078411@qq.com>
2017-04-01 00:11:44 +0800

    2017/4/1/0:11

--- app/src/main/java/com/example/darling/diary/DiaryActivity.java
+++ app/src/main/java/com/example/darling/diary/DiaryActivity.java
@@ -1,10 +1,17 @@
 package com.example.darling.diary;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import java.util.UUID;
 
 public class DiaryActivity extends SingleFragmentActivity {
-
+    public static final String EXTRA_EVENT_ID = "com.example.darling.diary.event_id";
+    public static Intent newIntent(Context packageContext, UUID eventId){
+        Intent intent = new Intent(packageContext,DiaryActivity.class);
+        intent.putExtra(EXTRA_EVENT_ID,eventId);
+        return intent;
+    }
    @Override
    protected Fragment createFragment(){
        return new EventFragment();
--- app/src/main/java/com/example/darling/diary/EventListFragment.java
+++ app/src/main/java/com/example/darling/diary/EventListFragment.java
@@ -1,5 +1,6 @@
 package com.example.darling.diary;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,7 +8,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -39,13 +44,31 @@ public class EventListFragment extends Fragment {
         mEventRecylerView.setAdapter(mAdapter);
     }
 
-    private class EventHolder extends RecyclerView.ViewHolder{
-        public TextView mTitleTextView;
+    private class EventHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private CheckBox mSolvedCheckBox;
+        private Event mEvent;
+
 
         public EventHolder(View itemView){
             super(itemView);
+            itemView.setOnClickListener(this);
 
-            mTitleTextView = (TextView)itemView;
+            mTitleTextView = (TextView)itemView.findViewById(R.id.list_item_event_title_text_view);
+            mDateTextView = (TextView) itemView.findViewById(R.id.list_item_date_text_view);
+            mSolvedCheckBox = (CheckBox) itemView.findViewById(R.id.list_item_event_solved_check_box);
+        }
+        public void bindEvent(Event event){
+            mEvent = event;
+            mTitleTextView.setText(mEvent.getTitle());
+            mDateTextView.setText(mEvent.getDate().toString());
+            mSolvedCheckBox.setChecked(mEvent.isSolved());
+        }
+        @Override
+        public void onClick(View v){
+            Intent intent = DiaryActivity.newIntent(getActivity(),mEvent.getId());
+            startActivity(intent);
         }
 
     }
@@ -58,13 +81,13 @@ public class EventListFragment extends Fragment {
         @Override
         public  EventHolder onCreateViewHolder(ViewGroup parent,int viewtype){
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1,parent,false);
+            View view = layoutInflater.inflate(R.layout.list_item_event,parent,false);
             return new EventHolder(view);
         }
         @Override
         public void onBindViewHolder(EventHolder holder,int position){
             Event event = mEvents.get(position);
-            holder.mTitleTextView.setText(event.getTitle());
+            holder.bindEvent(event);
         }
         @Override
         public int getItemCount(){
